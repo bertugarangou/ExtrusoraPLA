6d14aa48f16934a826a00a2f8555b42fc2a1e9da
Albert Garangou Culebras <42043974+bertugarangou@users.noreply.github.com>
2018-08-08 19:25:37 +0200

    rewrote to work with tb6600

--- Program/Extrusora/Extrusora.ino
+++ Program/Extrusora/Extrusora.ino
@@ -1,5 +1,6 @@
 /*
   Extrusora de PLA controlada per Arduino Mega 2560.
+  
   Projecte:
   - https://github.com/bertugarangou/ExtrusoraPLA
   Circuit:
@@ -12,35 +13,55 @@
     
 */
 /*+++++++++++++Llibreries++++++++++++++*/
-#include <Stepper.h>
 /*+++++++++++++Llibreries++++++++++++++*/
 
 /*++Declaració variables i constants+++*/
-Stepper extruder(200, 2, 3, 4, 5); //declara un motor pas-a-pas "extrusor" de "200" passos per rotació
-Stepper coil(200, 6, 7, 8, 9);  //declara un motor pas-a-pas "coil" de "200" passos per rotació
-int const hotTempOKLED = 10; //LED temperatura llesta per extrusió
-int const coldTempOKLED = 11; //LED temperatura llesta per maipulació
+//Pins components
+int const extruderDir = 2;  //motor extruder step
+int const extruderStep = 3;  //motor extruder canviar dir
+int const extruderEnable = 4; //motor extruder enable/holding o diable
+int const coilDir = 5; //motor coil step
+int const coilStep = 6;  //motor coil canviar dir
+int const coilEnable = 7; //motor coil enable/holding o disable
 
+int const hotTempOKLED = 8; //LED temperatura llesta per extrusió
+int const coldTempOKLED = 9; //LED temperatura llesta per maipulació
+int const switchFan = 10; //interruptor habilitar ventiladors
+int const switchHeater = 11;  //interruptor habilitar escalfador
+int const switchExtrude = 12; //interruptor motor extrusor
+int const switchWind = 13;  //interruptor motor bobina
+int const switchInvertDir = 14; //interruptor invertir direcció
 
-int const coldTempOKDegrees = 50; //temperatura llesta per manipulació
-int const hotTempOKDegrees = 200; // temperatura llesta per extrusió
-int const extremeTemp = 230; //temperatura màxima a la que pot arribar abans de activar refrigeració d'emergència.
+int const fanRelay = 15;  //relé ventiladors
+
+//variables temperatures
+int const coldTempOKDegrees = 50; //temperatura freda
+int const hotTempOKDegrees = 200; // temperatura calenta
+int const extremeTemp = 230; //temperatura massa calenta
 int currentTemp = 0; //temperatura actual actualitzada cada certs segons. Comença amb 0 per comprovar que s'actualitza, ja que mai arribarà als 0ºC
+
+int const timeToStopStep = 2;
+int const timeBetweenSteps = 2;
+//booleans per saber si es pot extrudir
+bool check_extrudeByRefrigeration = false;
+bool check_extrudeByTemp = false;
+bool check_extrudeBySwitch = false;
+
 /*++Declaració variables i constants+++*/
 
 /*+++++++++Configuració components+++++++++*/
 void setup() { //Declaració de components a la placa
-  extruder.setSpeed(100); //motor pas-a-pas a velocitat inicial indicada PPRPM
-  coil.setSpeed(100); //motor pas-a-pas de la bobina a velocitat inicial indicada PPRPM
-  
   pinMode(coldTempOKLED, OUTPUT); //LED temperatura llesta per maipulació
   pinMode(hotTempOKLED, OUTPUT);  //LED temperatura llesta per extrusió
+
 }
 /*+++++++++Configuració components+++++++++*/
 
 /*+++++++++++Declaracio funcions+++++++++++*/
-void coldTempOK();
-void callError();
+void tempAction(); //funció per dur a terme diverses accions depenent de la temperatura
+void callError(); //funció per escollir un missatge d'error i certes accions al respecte quan es cridi amb un codi d'error
+void toggleRefrigeration();
+void doStep();
 /*+++++++++++Declaracio funcions+++++++++++*/
 
 /*++++++++++++++++Processos++++++++++++++++*/
@@ -50,7 +71,55 @@ void loop() {
 /*++++++++++++++++Processos++++++++++++++++*/
 
 /*+++++++++++Definició funicons++++++++++++*/
-void TempAction(){
+void doStep(int motor, int dir){
+  switch (motor) {  //Estructura en switch per escollir direcció i motor
+                    //motor 0 = extrusor
+                    //motor 1 = bobina
+                    //direcció 0 = forward
+                    //direcció 1 = reverse
+    case 0:
+      switch (dir) {
+        case 0:
+          digitalWrite(extruderStep, HIGH);
+          delay(timeToStopStep);
+          digitalWrite(extruderStep, LOW);
+          delay(timeBetweenSteps);
+          break;
+          
+        case 1:
+          digitalWrite(extruderDir, HIGH);
+          digitalWrite(extruderStep, HIGH);
+          delay(timeToStopStep);
+          digitalWrite(extruderStep, LOW);
+          digitalWrite(extruderDir, LOW);
+          delay(timeBetweenSteps);
+        break;
+      }
+      break;
+
+    case 1:
+      switch (dir) {
+        case 0:
+          digitalWrite(coilStep, HIGH);
+          delay(timeToStopStep);
+          digitalWrite(extruderStep, LOW);
+          delay(timeBetweenSteps);
+          break;
+          
+        case 1:
+          digitalWrite(coilDir, HIGH);
+          digitalWrite(coilStep, HIGH);
+          delay(timeToStopStep);
+          digitalWrite(coilStep, LOW);
+          digitalWrite(coilDir, LOW);
+          delay(timeBetweenSteps);
+          break;
+      }
+      break;
+    }
+}
+
+void TempAction(){ //funció per dur a terme diverses accions depenent de la temperatura
   if(currentTemp < coldTempOKDegrees) {
     digitalWrite(coldTempOKLED, HIGH);
   }
@@ -69,7 +138,7 @@ void TempAction(){
   }
 }
 
-void callError(int errorCode){
+void callError(int errorCode){//funció per escollir un missatge d'error i certes accions al respecte quan es cridi amb un codi d'error
   switch (errorCode){
     case(0):
     //print on screen "ERROR 0"
@@ -77,6 +146,19 @@ void callError(int errorCode){
     case(1):
       //print on screen "Temp. massa alta"
       break;
+    case(2):
+    //print on screen "Temp no disponible"
+      break;
+  }
+}
+
+void toggleRefrigeration(){
+  if(digitalRead(switchFan) == HIGH){
+    digitalWrite(fanRelay, HIGH);
+    check_extrudeByRefrigeration = true;
+  }
+  else {
+    digitalWrite(fanRelay, LOW);
   }
 }
 
