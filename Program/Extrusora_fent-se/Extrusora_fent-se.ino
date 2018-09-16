c4f35be85dd7724a3c3277f37dbfd265eaceab6f
Albert Garangou Culebras <42043974+bertugarangou@users.noreply.github.com>
2018-09-16 12:59:11 +0200

    updated program directories

---
+++ Program/Extrusora_fent-se/Extrusora_fent-se.ino
@@ -0,0 +1,35 @@
+/*
+  Extrusora de PLA controlada per Arduino Mega 2560.
+
+  Projecte:
+  - https://github.com/bertugarangou/ExtrusoraPLA
+  Circuit:
+  - Extrusora feta per Albert Garangou,
+    com a Treball de Recerca a 2n de Batxillerat,
+    curs 2018/2019, tutor: Jordi Fanals Oriol,
+    codi amb Arduino IDE 1.8.5,
+    Copyright (c) 2018 Albert Garangou Culebras (albertgarangou@gmail.com)
+    Aquest codi està llicenciat sota la SIL Open Font License, Version 1.1.
+
+*/
+/*+++++++++++++Llibreries++++++++++++++*/
+#include <max6675.h>
+/*+++++++++++++Llibreries++++++++++++++*/
+/*++Declaració variables i constants+++*/
+
+/*++Declaració variables i constants+++*/
+/*+++++++++++Declaracio funcions+++++++++++*/
+
+/*+++++++++++Declaracio funcions+++++++++++*/
+/*+++++++++++Declaracio funcions+++++++++++*/
+
+void setup{
+  
+}
+
+void loop{
+  
+}
+/*+++++++++++Definició funicons++++++++++++*/
+/*+++++++++++Definició funicons++++++++++++*/
+
