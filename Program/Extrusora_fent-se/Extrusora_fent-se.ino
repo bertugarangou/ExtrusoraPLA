bbebd55f6fcab6304e5c2415c922785666584a4e
Albert Garangou Culebras <42043974+bertugarangou@users.noreply.github.com>
2018-09-30 12:06:02 +0200

    sistema d'alerta sonora activa

--- Program/Extrusora_fent-se/Extrusora_fent-se.ino
+++ Program/Extrusora_fent-se/Extrusora_fent-se.ino
@@ -7,7 +7,7 @@
   - Extrusora feta per Albert Garangou,
     com a Treball de Recerca a 2n de Batxillerat,
     curs 2018/2019, tutor: Jordi Fanals Oriol,
-    codi amb Arduino IDE 1.8.5,
+    codi amb Arduino IDE
     Copyright (c) 2018 Albert Garangou Culebras (albertgarangou@gmail.com)
     Aquest codi està llicenciat sota la SIL Open Font License, Version 1.1.
 
@@ -16,23 +16,28 @@
 #include <max6675.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2); //PINS SDA i SCL
+LiquidCrystal_I2C lcd(0x27, 16, 2); //PINS SDA i SCL lcd
 /*+++++++++++++Llibreries++++++++++++++*/
 /*++Declaració variables i constants+++*/
-int const INTFanFil = 53;
-int const INTFanTube = 52;
-int const RelayFanFil = 2;
-int const RelayFanTube = 3;
-int const STOPBtn = 13;
+unsigned long ultimMillis_LCDMain = 0UL;
+int const INTFanFil = 7;
+int const INTFanTube = 6;
+int const RelayFanFil = 52;
+int const RelayFanTube = 50;
+int const STOPBtn = 30;
+int const brunzidor = 51;
 
 bool error = false;
-#define HIGH 0x0 //0 = HIGH     HIGH per quan 1 no activa
-#define LOW 0x1 // 1 = LOW      1 per normal
+bool canExtrude = false;
+int currentTemp = 120; //FAKEEEEE
+int desiredTemp = 190; //FAKEEE
+int const stepperSpeed = 1.5; //0.5-100
+int const timeBetweenSteps = 1.5; //0-100
 /*++Declaració variables i constants+++*/
 /*+++++++++++Declaracio funcions+++++++++++*/
+void lcdController();
+/*+++++++++++Declaracio funcions+++++++++++*/
 
-/*+++++++++++Declaracio funcions+++++++++++*/
-/*+++++++++++Declaracio funcions+++++++++++*/
 
 void setup(){
   Serial.begin(9600); //inicia la depuració
@@ -44,11 +49,12 @@ void setup(){
   pinMode(STOPBtn, INPUT);
   pinMode(RelayFanFil, OUTPUT);
   pinMode(RelayFanTube, OUTPUT);
-    lcd.print("HI!!!");
+  pinMode(brunzidor, OUTPUT);
 }
 
 void loop(){
  if(digitalRead(STOPBtn) == 0 || error == true){
+  digitalWrite(brunzidor, HIGH);
   Serial.println("*****************************************");
   Serial.print("*   STOP");
   Serial.println(", entrant en mode emergència!   *");
@@ -56,36 +62,66 @@ void loop(){
   Serial.println("* NO DESCONECTAR FINS QUE ESTIGUI FRED! *");
   Serial.println("*****************************************");
   lcd.clear();
-  lcd.print("   ALERTA !!!");
+  lcd.print(" !!! ALERTA !!!");
   lcd.setCursor(0,1);
+  lcd.print(" NO DESCONECTAR");
   //procedimentError();   //necessàri o només caldria missatge pantalla diferent?
   digitalWrite(RelayFanFil, HIGH); //HIGH!?!?!?
   digitalWrite(RelayFanTube, HIGH);  //HIGH!?!?!
   while(true){//bucle infinit
   lcd.noBacklight();
+  digitalWrite(brunzidor, LOW);
   delay(500);
+  digitalWrite(brunzidor, HIGH);
   lcd.backlight();
   delay(2000);
   }
  }
-  else{
-
-    if(digitalRead(INTFanFil) == HIGH){
-      digitalWrite(RelayFanFil, 1);
+  else{ //funcionament estandart del programa (aka no hi ha cap error)
+    //Serial.print("Temp: "); Serial.print(currentTemp); Serial.print("/"); Serial.println(desiredTemp);  //enviar per Serial la temperatura
+    
+    if(digitalRead(INTFanFil) == HIGH){ //quan s'activa l'interruptor adequat
+      digitalWrite(RelayFanFil, 0); //activar relé ventilador
+      //Serial.println("Ventilador: Filament **ON**");
     }
-    else{
-      digitalWrite(RelayFanFil, 0);
+    else{ //sinó
+      digitalWrite(RelayFanFil, 1); //desactiva'l
+      //Serial.println("Ventilador: Filament **OFF**");
     }
     
-    if(digitalRead(INTFanTube) == HIGH){
-      digitalWrite(RelayFanTube, 1);
+    if(digitalRead(INTFanTube) == HIGH){  //si s'activa l'interruptor adequat
+      digitalWrite(RelayFanTube, 0);  //activa el relé del ventilador
+      //Serial.println("Ventilador: Extrusora **ON**");
     }
-    else{
-      digitalWrite(RelayFanTube, 0);
+    else{ //sinó
+      digitalWrite(RelayFanTube, 1);  //desactiva'l
+      //Serial.println("Ventilador: Extrusora **OFF**");
     }
  }
-}
-/*+++++++++++Definició funicons++++++++++++*/
-/*+++++++++++Definició funicons++++++++++++*/
+ lcdController();
+} //end
 
-
+/*+++++++++++Definició funicons++++++++++++*/
+  void lcdController(){
+    if(millis() - ultimMillis_LCDMain >= 2000UL){
+      lcd.setCursor(0,0);
+      lcd.print(currentTemp);
+      lcd.print("/");
+      lcd.print(desiredTemp);
+      lcd.print(char(223));
+      lcd.print("  ");
+      
+      if(canExtrude == true){
+        lcd.setCursor(10,0);
+        lcd.print(" LLEST");
+      }
+      else{
+        lcd.setCursor(10,0);
+        lcd.print("ESPERA");
+      }
+      
+      lcd.print(millis());
+      ultimMillis_LCDMain = millis();
+    }
+  }
+/*+++++++++++Definició funicons++++++++++++*/
