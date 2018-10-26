b72f617387a0e31955e3ba91fdee4a51730f7f16
Enric <44509882+Mntain@users.noreply.github.com>
2018-10-26 21:51:43 +0200

    llicència afegida

--- Program/tests/Comprovador_estat_INT/Comprovador_estat_INT.ino
+++ Program/tests/Comprovador_estat_INT/Comprovador_estat_INT.ino
@@ -1,3 +1,16 @@
+/*
+  Extrusora de PLA controlada per Arduino Mega 2560.
+  Projecte:
+  - https://github.com/bertugarangou/ExtrusoraPLA
+  - Extrusora feta per Albert Garangou,
+    com a Treball de Recerca a 2n de Batxillerat,
+    curs 2018/2019, tutor: Jordi Fanals Oriol,
+    codi amb Atom i Arduino-upload package.
+  - Tots els drets reservats Albert Garangou Culebras (albertgarangou@gmail.com).
+    Aquest codi és conegut com a "Proprietary software".
+    Consulta el web origen per a més informació.
+*/
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(49, INPUT);
